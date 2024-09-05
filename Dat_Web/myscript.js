26b92d12cd6bb364da34e20ca1b89e4e6ff23e9e
Lisa Pham <lphm@iastate.edu>
2024-09-04 20:42:46 -0500

    Add files via upload

---
+++ Dat_Web/myscript.js
@@ -0,0 +1,46 @@
+const openSidebar = () => {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+};
+
+const allSidebar = () =>{
+  document.getElementById("mySidebar").style.width = "500px";
+  document.getElementById("main").style.marginLeft = "500px";
+}
+
+const hideSidebar = () => {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+};
+
+
+function scrollWin() {
+
+  window.scrollTo(0,500);
+}
+
+// FoodTo Scroller
+
+const Scrollers = document.querySelectorAll(".PhotoScroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation(){
+  
+  Scrollers.forEach((PhotoScroller) => {
+    PhotoScroller.setAttribute("data-animated", true);
+
+    const ScrollerInner = PhotoScroller.querySelector(".scroller_inner");
+    const ScrollerContent = Array.from(ScrollerInner.children);
+
+    ScrollerContent.forEach((item) =>{
+      const duplicatedItem = item.cloneNode(true);
+
+      duplicatedItem.setAttribute("aria-hidden", true);
+      ScrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
